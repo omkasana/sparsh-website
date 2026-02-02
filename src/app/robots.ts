76261ba929c3a7715442e2fce5161ecc7ffa3230
om kasana <omkasana@oms-MacBook-Air.local>
2026-02-02 18:41:23 +0530

    robots.txt edit

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,44 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules:[
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+      {
+        userAgent: "Bingbot",
+        allow: "/",
+      },
+      {
+        userAgent: "YandexBot",
+        allow: "/",
+      },
+      {
+        userAgent: "GPTBot", // ChatGPT
+        allow: "/",
+      },
+      {
+        userAgent: "ChatGPT-User", // ChatGPT Search
+        allow: "/",
+      },
+      {
+        userAgent: "ClaudeBot", // Anthropic Claude
+        allow: "/",
+      },
+      {
+        userAgent: "PerplexityBot", // Perplexity AI
+        allow: "/",
+      },
+      {
+        userAgent: "Google-Extended", // Google Gemini/Bard
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        disallow: "/",
+      },
+    ],
     sitemap: "https://sparshpolymer.com/sitemap.xml",
   };
 }
